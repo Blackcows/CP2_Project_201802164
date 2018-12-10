890d4bbcbec986ba7dde2d22d784e28e4837b6bd
Blackcows <sda6206@gmail.com>
2018-12-10 22:05:50 +0900

    테스트 겸 1차 수정 (mainScreen 변경)

--- src/program_screen/MainScreen.java
+++ src/program_screen/MainScreen.java
@@ -8,11 +8,21 @@ import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class MainScreen extends JFrame {
 	
 	private Graphics g;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
 
 	/**
 	 * Launch the application.
@@ -41,82 +51,108 @@ public class MainScreen extends JFrame {
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		JLabel lblNewLabel = new JLabel("\uC774\uBBF8\uC9C0");
-		lblNewLabel.setBackground(Color.WHITE);
-		lblNewLabel.setForeground(Color.BLACK);
-		lblNewLabel.setBounds(77, 90, 200, 250);
-		contentPane.add(lblNewLabel);
+		JLabel lbl_image1 = new JLabel("이미지");
+		lbl_image1.setBackground(Color.WHITE);
+		lbl_image1.setForeground(Color.BLACK);
+		lbl_image1.setBounds(77, 90, 200, 250);
+		contentPane.add(lbl_image1);
 		
-		JLabel lblNewLabel_1 = new JLabel("\uC81C\uBAA9");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(100, 350, 150, 30);
-		contentPane.add(lblNewLabel_1);
+		JLabel lbl_image2 = new JLabel("이미지");
+		lbl_image2.setBounds(353, 90, 200, 250);
+		contentPane.add(lbl_image2);
 		
-		JLabel lblNewLabel_2 = new JLabel("\uD3C9\uC810");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(100, 394, 70, 30);
-		contentPane.add(lblNewLabel_2);
+		JLabel lbl_image3 = new JLabel("이미지");
+		lbl_image3.setBounds(678, 90, 200, 250);
+		contentPane.add(lbl_image3);
 		
-		JLabel label = new JLabel("\uC608\uB9E4\uC728");
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setBounds(180, 394, 70, 30);
-		contentPane.add(label);
+		JLabel lbl1 = new JLabel("오늘의 추천작을 만나보세요~!");
+		lbl1.setBounds(77, 44, 200, 30);
+		contentPane.add(lbl1);
 		
-		JLabel label_1 = new JLabel("\uC774\uBBF8\uC9C0");
-		label_1.setBounds(353, 90, 200, 250);
-		contentPane.add(label_1);
+		JLabel lbl2 = new JLabel("(Name) 님 환영합니다.");
+		lbl2.setBounds(12, 10, 200, 30);
+		contentPane.add(lbl2);
 		
-		JLabel label_2 = new JLabel("\uC774\uBBF8\uC9C0");
-		label_2.setBounds(678, 90, 200, 250);
-		contentPane.add(label_2);
+		JButton marketButton = new JButton("상점");
+		marketButton.setBounds(815, 14, 60, 30);
+		contentPane.add(marketButton);
 		
-		JLabel label_3 = new JLabel("\uC81C\uBAA9");
-		label_3.setHorizontalAlignment(SwingConstants.CENTER);
-		label_3.setBounds(372, 350, 150, 30);
-		contentPane.add(label_3);
+		JPanel imagePanel = new CirclePanel();
+		imagePanel.setBounds(884, 10, 90, 86);
+		contentPane.add(imagePanel);
+		imagePanel.setLayout(null);
 		
-		JLabel label_4 = new JLabel("\uD3C9\uC810");
-		label_4.setHorizontalAlignment(SwingConstants.CENTER);
-		label_4.setBounds(372, 394, 70, 30);
-		contentPane.add(label_4);
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setText("제목");
+		textField.setEditable(false);
+		textField.setBounds(100, 350, 150, 30);
+		contentPane.add(textField);
+		textField.setColumns(10);
 		
-		JLabel label_5 = new JLabel("\uC608\uB9E4\uC728");
-		label_5.setHorizontalAlignment(SwingConstants.CENTER);
-		label_5.setBounds(452, 394, 70, 30);
-		contentPane.add(label_5);
+		textField_1 = new JTextField();
+		textField_1.setText("제목");
+		textField_1.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_1.setEditable(false);
+		textField_1.setColumns(10);
+		textField_1.setBounds(372, 350, 150, 30);
+		contentPane.add(textField_1);
 		
-		JLabel label_6 = new JLabel("\uC81C\uBAA9");
-		label_6.setHorizontalAlignment(SwingConstants.CENTER);
-		label_6.setBounds(702, 350, 150, 30);
-		contentPane.add(label_6);
+		textField_2 = new JTextField();
+		textField_2.setText("제목");
+		textField_2.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		textField_2.setBounds(702, 350, 150, 30);
+		contentPane.add(textField_2);
 		
-		JLabel label_7 = new JLabel("\uD3C9\uC810");
-		label_7.setHorizontalAlignment(SwingConstants.CENTER);
-		label_7.setBounds(702, 394, 70, 30);
-		contentPane.add(label_7);
+		textField_3 = new JTextField();
+		textField_3.setText("평점");
+		textField_3.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_3.setEditable(false);
+		textField_3.setColumns(10);
+		textField_3.setBounds(100, 394, 70, 30);
+		contentPane.add(textField_3);
 		
-		JLabel label_8 = new JLabel("\uC608\uB9E4\uC728");
-		label_8.setHorizontalAlignment(SwingConstants.CENTER);
-		label_8.setBounds(782, 394, 70, 30);
-		contentPane.add(label_8);
+		textField_4 = new JTextField();
+		textField_4.setText("평점");
+		textField_4.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_4.setEditable(false);
+		textField_4.setColumns(10);
+		textField_4.setBounds(372, 394, 70, 30);
+		contentPane.add(textField_4);
 		
-		JLabel lblNewLabel_3 = new JLabel("\uC624\uB298\uC758 \uCD94\uCC9C\uC791\uC744 \uB9CC\uB098\uBCF4\uC138\uC694~!");
-		lblNewLabel_3.setToolTipText("");
-		lblNewLabel_3.setBounds(77, 44, 200, 30);
-		contentPane.add(lblNewLabel_3);
+		textField_5 = new JTextField();
+		textField_5.setText("평점");
+		textField_5.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_5.setEditable(false);
+		textField_5.setColumns(10);
+		textField_5.setBounds(702, 394, 70, 30);
+		contentPane.add(textField_5);
 		
-		JLabel lblNewLabel_4 = new JLabel("(Name) \uB2D8 \uD658\uC601\uD569\uB2C8\uB2E4");
-		lblNewLabel_4.setBounds(12, 10, 200, 30);
-		contentPane.add(lblNewLabel_4);
+		textField_6 = new JTextField();
+		textField_6.setText("예매율");
+		textField_6.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_6.setEditable(false);
+		textField_6.setColumns(10);
+		textField_6.setBounds(180, 394, 70, 30);
+		contentPane.add(textField_6);
 		
-		JButton btnNewButton = new JButton("\uC0C1\uC810");
-		btnNewButton.setBounds(815, 14, 60, 30);
-		contentPane.add(btnNewButton);
+		textField_7 = new JTextField();
+		textField_7.setText("예매율");
+		textField_7.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_7.setEditable(false);
+		textField_7.setColumns(10);
+		textField_7.setBounds(452, 394, 70, 30);
+		contentPane.add(textField_7);
 		
-		JPanel panel = new CirclePanel();
-		panel.setBounds(884, 10, 90, 86);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		textField_8 = new JTextField();
+		textField_8.setText("예매율");
+		textField_8.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_8.setEditable(false);
+		textField_8.setColumns(10);
+		textField_8.setBounds(782, 394, 70, 30);
+		contentPane.add(textField_8);
 	}
 }
 
